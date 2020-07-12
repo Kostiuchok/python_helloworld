18f9ea4865be14908fb0c2256414b0119d5bd30a
Tolik <tolik.kostyuchok@gmail.com>
2020-07-12 12:07:28 +0300

    firs commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+# x = 5  # this may look
+# y = 3
+# print(x+y)
+# print(x-y)
+# print(x/y)
+# print(x//y)
+# print(x*y)
+# print(x**y)
+
+# calculating of credit final sum
+
+credit = 7000
+credit_rate = 7
+number_of_years = 9
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+
+print(final_sum)
